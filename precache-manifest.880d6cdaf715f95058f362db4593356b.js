34060784ab866b734a980fd0995a002c156d3ad4
Freddy Alejandro <facuellarg@unal.edu.co>
2020-04-26 11:59:48 -0500

    Updates

---
+++ precache-manifest.880d6cdaf715f95058f362db4593356b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7473ef1b555db7cfad759557b385d30a",
+    "url": "/homepage/index.html"
+  },
+  {
+    "revision": "85e02b6ffc42b6a87303",
+    "url": "/homepage/static/css/main.691d6799.chunk.css"
+  },
+  {
+    "revision": "7ea590c961dbf509a814",
+    "url": "/homepage/static/js/2.bf0ef409.chunk.js"
+  },
+  {
+    "revision": "e73ed4795920cd1b49de22c460f2ce43",
+    "url": "/homepage/static/js/2.bf0ef409.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85e02b6ffc42b6a87303",
+    "url": "/homepage/static/js/main.3bfb6a94.chunk.js"
+  },
+  {
+    "revision": "641f775d676877eb4c2a",
+    "url": "/homepage/static/js/runtime-main.c79b4a05.js"
+  },
+  {
+    "revision": "8cb69e6cb0b9bc4c2b88b82bfa13da7c",
+    "url": "/homepage/static/media/background-tile.8cb69e6c.png"
+  },
+  {
+    "revision": "c71c3ba6a2759111c77a0c9ffff41e84",
+    "url": "/homepage/static/media/fire.c71c3ba6.jpg"
+  }
+]);
